82a89e90cfc06286c8b094ac7cc752f45d53d7bc
anaelG2 <anael.guillaumot@outlook.com>
2022-06-27 10:32:04 +0200

    remove zones management

--- main.tf
+++ main.tf
@@ -8,15 +8,6 @@ terraform {
   required_version = ">= 0.13"
 }
 
-### Création du bucket
-resource "scaleway_object_bucket" "website-bucket" {
-  name = "${var.projet_name}-${var.scaleway_bucket_name}"
-  acl  = "public-read"
-  tags = {
-    key = "websitebucket"
-  }
-}
-
 ### Ajout clé ssh au compte Scaleway
 resource "scaleway_account_ssh_key" "main" {
   name       = "main"
@@ -27,7 +18,7 @@ resource "scaleway_account_ssh_key" "main" {
 
 ### Création de l'adresse ip du loadbalancer et du loadbalancer
 resource "scaleway_lb_ip" "loadbalancer-ip" {
-  zone       = var.scaleway_zone_1
+  zone       = var.scaleway_zone
   project_id = var.scaleway_project_id
 }
 resource "scaleway_lb" "loadbalancer" {
@@ -43,143 +34,72 @@ resource "scaleway_lb" "loadbalancer" {
 
 
 ### Création des groupes de sécurité pour les instances des zones 1 et 2
-resource "scaleway_instance_security_group" "scaleway_security_group_zone_1" {
+resource "scaleway_instance_security_group" "scaleway_security_group_zone" {
   project_id              = var.scaleway_project_id
-  zone                    = var.scaleway_zone_1
-  name                    = "security-group-${var.scaleway_region_1}"
-  inbound_default_policy  = "accept"
-  outbound_default_policy = "accept"
-}
-resource "scaleway_instance_security_group" "scaleway_security_group_zone_2" {
-  project_id              = var.scaleway_project_id
-  zone                    = var.scaleway_zone_2
-  name                    = "security-group-${var.scaleway_region_2}"
+  zone                    = var.scaleway_zone
+  name                    = "security-group-${var.scaleway_region}"
   inbound_default_policy  = "accept"
   outbound_default_policy = "accept"
 }
 
 
 ### Création des IP et instances
-resource "scaleway_instance_ip" "public_ip_zone_1" {
-  count      = var.instance_count_zone_1
+resource "scaleway_instance_ip" "public_ip_zone" {
+  count      = var.instance_count
   project_id = var.scaleway_project_id
-  zone       = var.scaleway_zone_1
+  zone       = var.scaleway_zone
 }
-resource "scaleway_instance_server" "nodes-zone-1" {
-  count             = var.instance_count_zone_1
+resource "scaleway_instance_server" "nodes" {
+  count             = var.instance_count
   project_id        = var.scaleway_project_id
-  zone              = var.scaleway_zone_1
-  name              = "${var.projet_name}-${var.webserver_node_name}-${var.scaleway_region_1}-${count.index + 1}"
+  zone              = var.scaleway_zone
+  name              = "${var.projet_name}-${var.webserver_node_name}-${var.scaleway_region}-${count.index + 1}"
   type              = var.instance_type
   image             = "debian_bullseye"
   tags              = ["node", "webserver", "projet-annuel"]
-  ip_id             = "${scaleway_instance_ip.public_ip_zone_1[count.index].id}"
-  security_group_id = scaleway_instance_security_group.scaleway_security_group_zone_1.id
+  ip_id             = "${scaleway_instance_ip.public_ip_zone[count.index].id}"
+  security_group_id = scaleway_instance_security_group.scaleway_security_group_zone.id
   depends_on = [
     scaleway_lb.loadbalancer,
-    scaleway_instance_security_group.scaleway_security_group_zone_1,
-    scaleway_instance_ip.public_ip_zone_1,
-    scaleway_object_bucket.website-bucket
+    scaleway_instance_security_group.scaleway_security_group_zone,
+    scaleway_instance_ip.public_ip_zone
   ]
 }
 
-
-### Création des IP et instances
-resource "scaleway_instance_ip" "public_ip_zone_2" {
-  count      = var.instance_count_zone_2
-  project_id = var.scaleway_project_id
-  zone       = var.scaleway_zone_2
-}
-resource "scaleway_instance_server" "nodes-zone-2" {
-  count             = var.instance_count_zone_2
-  project_id        = var.scaleway_project_id
-  zone              = var.scaleway_zone_2
-  name              = "${var.projet_name}-${var.webserver_node_name}-${var.scaleway_region_2}-${count.index + 1}"
-  type              = var.instance_type
-  image             = "debian_bullseye"
-  tags              = ["node", "webserver", "projet-annuel"]
-  ip_id             = "${scaleway_instance_ip.public_ip_zone_2[count.index].id}"
-  security_group_id = scaleway_instance_security_group.scaleway_security_group_zone_2.id
-  depends_on = [
-    scaleway_lb.loadbalancer,
-    scaleway_instance_security_group.scaleway_security_group_zone_2,
-    scaleway_instance_ip.public_ip_zone_2,
-    scaleway_object_bucket.website-bucket,
-  ]
-}
-
-
-### Création des frontends pour les zones 1 et 2
-resource "scaleway_lb_frontend" "frontend-zone-1" {
-  count        = var.instance_count_zone_1
-  lb_id        = scaleway_lb.loadbalancer.id
-  backend_id   = "${scaleway_lb_backend.backend-zone-1[count.index].id}"
-  name         = "${var.projet_name}-zone-1-frontend-${count.index + 1}"
-  inbound_port = 80
-  depends_on   = [scaleway_lb.loadbalancer, scaleway_instance_server.nodes-zone-1]
-}
-resource "scaleway_lb_frontend" "frontend-zone-2" {
-  count        = var.instance_count_zone_2
-  lb_id        = scaleway_lb.loadbalancer.id
-  backend_id   = "${scaleway_lb_backend.backend-zone-2[count.index].id}"
-  name         = "${var.projet_name}-zone-2-frontend-${count.index + 1}"
-  inbound_port = 80
-  depends_on   = [scaleway_lb.loadbalancer, scaleway_instance_server.nodes-zone-2]
-}
-
-
-### Création des backends pour les zones 1 et 2
-resource "scaleway_lb_backend" "backend-zone-1" {
-  count            = var.instance_count_zone_1
+resource "scaleway_lb_backend" "backend" {
   lb_id            = scaleway_lb.loadbalancer.id
-  name             = "${var.projet_name}-zone-1-backend-${count.index + 1}"
+  name             = "${var.projet_name}-backend"
   forward_protocol = "tcp"
   forward_port     = 80
   proxy_protocol   = "none"
-  server_ips       = "${scaleway_instance_server.nodes-zone-1.*.public_ip}"
-  depends_on       = [scaleway_lb.loadbalancer, scaleway_instance_server.nodes-zone-1]
+  server_ips       = "${scaleway_instance_server.nodes.*.public_ip}"
+  depends_on       = [scaleway_lb.loadbalancer, scaleway_instance_server.nodes]
 }
-resource "scaleway_lb_backend" "backend-zone-2" {
-  count            = var.instance_count_zone_2
-  lb_id            = scaleway_lb.loadbalancer.id
-  name             = "${var.projet_name}-zone-1-backend-${count.index + 1}"
-  forward_protocol = "tcp"
-  forward_port     = 80
-  proxy_protocol   = "none"
-  server_ips       = "${scaleway_instance_server.nodes-zone-2.*.public_ip}"
-  depends_on       = [scaleway_lb.loadbalancer, scaleway_instance_server.nodes-zone-2]
+resource "scaleway_lb_frontend" "frontend" {
+  # count        = var.instance_count
+  lb_id        = scaleway_lb.loadbalancer.id
+  backend_id   = "${scaleway_lb_backend.backend.id}"
+  name         = "${var.projet_name}-frontend}"
+  inbound_port = 80
+  depends_on   = [scaleway_lb_backend.backend]
 }
 
 
 ### Création de la route des frontends aux backends pour les zones 1 et 2
-resource "scaleway_lb_route" "route-zone-1" {
-  count       = var.instance_count_zone_1
-  frontend_id = "${scaleway_lb_frontend.frontend-zone-1[count.index].id}"
-  backend_id  = "${scaleway_lb_backend.backend-zone-1[count.index].id}"
+resource "scaleway_lb_route" "route" {
+  frontend_id = "${scaleway_lb_frontend.frontend.id}"
+  backend_id  = "${scaleway_lb_backend.backend.id}"
   match_sni   = "scaleway.com"
+  depends_on = [scaleway_lb_frontend.frontend]
 }
-resource "scaleway_lb_route" "route-zone-2" {
-  count       = var.instance_count_zone_2
-  frontend_id = "${scaleway_lb_frontend.frontend-zone-2[count.index].id}"
-  backend_id  = "${scaleway_lb_backend.backend-zone-2[count.index].id}"
-  match_sni   = "scaleway.com"
-}
-
 
 # Output des valeurs à passer au fichier output.tf (IP et Nom des instance des zones 1 et 2)
-output "webserver_names_zone_1" {
-  value = ["${scaleway_instance_server.nodes-zone-1.*.name}"]
+output "webserver_names_zone" {
+  value = ["${scaleway_instance_server.nodes.*.name}"]
 }
-output "webserver_ips_zone_1" {
-  value = ["${scaleway_instance_server.nodes-zone-1.*.public_ip}"]
+output "webserver_ips_zone" {
+  value = ["${scaleway_instance_server.nodes.*.public_ip}"]
 }
-output "webserver_names_zone_2" {
-  value = ["${scaleway_instance_server.nodes-zone-2.*.name}"]
-}
-output "webserver_ips_zone_2" {
-  value = ["${scaleway_instance_server.nodes-zone-2.*.public_ip}"]
-}
-
 
 
 # Affichage de l'adresse IP publique du loadbalancer
--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,12 @@
-### Création et mise à jour de l'inventaire hosts-zone-1
-resource "local_file" "ansible-inventory-zone-1" {
-  count = var.instance_count_zone_1
+### Création et mise à jour de l'inventaire hosts
+resource "local_file" "ansible-inventory" {
+  count = var.instance_count
   content = templatefile("./templates/ansible_inventory.tmpl", {
-    hostnames     = "${scaleway_instance_server.nodes-zone-1.*.name}"
-    ansible_hosts = "${scaleway_instance_server.nodes-zone-1.*.public_ip}"
+    hostnames     = "${scaleway_instance_server.nodes.*.name}"
+    ansible_hosts = "${scaleway_instance_server.nodes.*.public_ip}"
     }
   )
-  filename   = "./hosts-zone-1"
-  depends_on = [scaleway_instance_server.nodes-zone-1]
+  filename   = "./hosts"
+  depends_on = [scaleway_instance_server.nodes]
 }
 
-
-### Création et mise à jour de l'inventaire hosts-zone-2
-resource "local_file" "ansible-inventory-zone-2" {
-  count = var.instance_count_zone_2
-  content = templatefile("./templates/ansible_inventory.tmpl", {
-    hostnames     = "${scaleway_instance_server.nodes-zone-2.*.name}"
-    ansible_hosts = "${scaleway_instance_server.nodes-zone-2.*.public_ip}"
-    }
-  )
-  filename   = "./hosts-zone-2"
-  depends_on = [scaleway_instance_server.nodes-zone-2]
-}
